e4deca631d14658fb9f4a91793037ce3e92f6a5a
SetnameWang <32866865+SetnameWang@users.noreply.github.com>
2019-03-07 21:12:03 +1100

    Add files via upload

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 webConfig = {
     'webPath' : '\\webBase\\root',
     'osSep' : '\\',
-    'blockTemplateName' : True
+    'blockTemplateName' : True,
+    'fileAllow' : 'all',
+    ##'fileAllow' : ('.jps', '.txt')
+    'allowUnknowFile' : True
     }
