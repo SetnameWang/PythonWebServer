79b251a55150db73f9c96b41c39bc7320f875e9f
SetnameWang <32866865+SetnameWang@users.noreply.github.com>
2019-03-07 21:11:17 +1100

    Add files via upload

--- webBase/loadStasticFile.py
+++ webBase/loadStasticFile.py
@@ -1,7 +1,9 @@
 class loadFile:
-    def __init__(self, path, block, debug):
+    def __init__(self, path, block, debug, config, handShake):
         self.path = path
         self.block = block
+        self.config = config
+        self.handShake = handShake
         self.info = '[{type}][webBase][{time}]:{info}'
         self.debug = debug
 
@@ -11,115 +13,45 @@ class loadFile:
         message = {}
         import time
         if self.debug == True: print(self.info.format(time = time.asctime(time.localtime(time.time())), type = 'Debug', info = 'Try to open file ' + self.path))
-        if '.jpg' in self.path:
+        try:
+            file = open(self.path, mode='rb').read()
+        except:
+            if self.debug == True: print(self.info.format(time = time.asctime(time.localtime(time.time())), type = 'Debug', info = 'Can\'t found file.'))
+            return self.err404()
+        
+        if self.config['fileAllow'] == 'all':
+            if self.debug == True: print(self.info.format(time = time.asctime(time.localtime(time.time())), type = 'Debug', info = 'Loading file allow list.'))
+            import json
+            fileList = open(self.path.split(self.config['webPath'])[0] + self.config['webPath'] + self.config['osSep'] + 'fileList.json', 'r',encoding='utf-8')
+            list = fileList.read()
+            fileList.close()
+            list = json.loads(list)
+            message = {}
             try:
-                file = open(self.path, mode='rb').read()
+                suffPath = self.path.split('.')[-1]
+                patched = False
+                suffix = ''
+                for i in list['list']:
+                    if i in self.path:
+                        patched = True
+                        suffix = list['reflection'][i]
+                        break
             except:
-                if self.debug == True: print(self.info.format(time = time.asctime(time.localtime(time.time())), type = 'Debug', info = 'Can\'t uncode jpg file.'))
-                return self.err404()
-            if self.debug == True: print(self.info.format(time = time.asctime(time.localtime(time.time())), type = 'Debug', info = 'Success uncode jpg file.'))
+                if self.debug == True: print(self.info.format(time = time.asctime(time.localtime(time.time())), type = 'Debug', info = 'Can\'t patch suffix.'))
+                if self.config['allowUnknowFile'] == False: return self.err404()
+                suffix = 'application/octet-stream'
+                
             message['stateCode'] = '200'
             message['state'] = 'OK'
             import datetime
             message['date'] = datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
             message['bodyMessage'] = file
-            message['others'] = '\r\ncontent-type: image/jpg\r\naccept-ranges: bytes'
-            return message
-        elif '.png' in self.path:
-            try:
-                file = open(self.path, mode='rb').read()
-            except:
-                if self.debug == True: print(self.info.format(time = time.asctime(time.localtime(time.time())), type = 'Debug', info = 'Can\'t uncode png file.'))
-                return self.err404()
-            if self.debug == True: print(self.info.format(time = time.asctime(time.localtime(time.time())), type = 'Debug', info = 'Success uncode png file.'))
-            message['stateCode'] = '200'
-            message['state'] = 'OK'
-            import datetime
-            message['date'] = datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
-            message['bodyMessage'] = file
-            message['others'] = '\r\ncontent-type: image/png\r\naccept-ranges: bytes'
-            return message
-        elif '.gif' in self.path:
-            try:
-                file = open(self.path, mode='rb').read()
-            except:
-                if self.debug == True: print(self.info.format(time = time.asctime(time.localtime(time.time())), type = 'Debug', info = 'Can\'t uncode gif file.'))
-                return self.err404()
-            if self.debug == True: print(self.info.format(time = time.asctime(time.localtime(time.time())), type = 'Debug', info = 'Success uncode gif file.'))
-            message['stateCode'] = '200'
-            message['state'] = 'OK'
-            import datetime
-            message['date'] = datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
-            message['bodyMessage'] = file
-            message['others'] = '\r\ncontent-type: image/gif\r\naccept-ranges: bytes'
-            return message
-        elif '.ico' in self.path:
-            try:
-                file = open(self.path, mode='rb').read()
-            except:
-                print(self.path)
-                if self.debug == True: print(self.info.format(time = time.asctime(time.localtime(time.time())), type = 'Debug', info = 'Can\'t uncode ICO file.'))
-                return self.err404()
-            if self.debug == True: print(self.info.format(time = time.asctime(time.localtime(time.time())), type = 'Debug', info = 'Success uncode ICO file.'))
-            message['stateCode'] = '200'
-            message['state'] = 'OK'
-            import datetime
-            message['date'] = datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
-            message['bodyMessage'] = file
-            message['others'] = '\r\ncontent-type: image/x-icon\r\naccept-ranges: bytes'
+            message['others'] = '\r\ncontent-type: {type}\r\naccept-ranges: bytes'.format(type = suffix)
             return message
 
-        elif '.js' in self.path:
-            try:
-                file = open(self.path, mode='r',encoding='utf-8').read()
-            except:
-                if self.debug == True: print(self.info.format(time = time.asctime(time.localtime(time.time())), type = 'Debug', info = 'Can\'t uncode js file.'))
-                return self.err404()
-            if self.debug == True: print(self.info.format(time = time.asctime(time.localtime(time.time())), type = 'Debug', info = 'Success uncode js file.'))
-            message['stateCode'] = '200'
-            message['state'] = 'OK'
-            import datetime
-            message['date'] = datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
-            message['bodyMessage'] = bytes(file,encoding='utf-8')
-            message['others'] = '\r\ncontent-type: text/javascript'
-            return message
-        elif '.css' in self.path:
-            try:
-                file = open(self.path, mode='r',encoding='utf-8').read()
-            except:
-                if self.debug == True: print(self.info.format(time = time.asctime(time.localtime(time.time())), type = 'Debug', info = 'Can\'t uncode css file.'))
-                return self.err404()
-            if self.debug == True: print(self.info.format(time = time.asctime(time.localtime(time.time())), type = 'Debug', info = 'Success uncode css file.'))
-            message['stateCode'] = '200'
-            message['state'] = 'OK'
-            import datetime
-            message['date'] = datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
-            message['bodyMessage'] = bytes(file,encoding='utf-8')
-            message['others'] = '\r\ncontent-type: text/css'
-            return message
-        elif '.html' in self.path:
-            try:
-                file = open(self.path, mode='r',encoding='utf-8').read()
-            except:
-                if self.debug == True: print(self.info.format(time = time.asctime(time.localtime(time.time())), type = 'Debug', info = 'Can\'t uncode html file.'))
-                return self.err404()
-            if self.debug == True: print(self.info.format(time = time.asctime(time.localtime(time.time())), type = 'Debug', info = 'Success uncode html file.'))
-            message['stateCode'] = '200'
-            message['state'] = 'OK'
-            import datetime
-            message['date'] = datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
-            message['bodyMessage'] = bytes(file,encoding='utf-8')
-            message['others'] = '\r\ncontent-type: text/html'
-            return message
         else:
-            if self.debug == True: print(self.info.format(time = time.asctime(time.localtime(time.time())), type = 'Debug', info = 'Can\'t unexcept path.'))
-            message['stateCode'] = '400'
-            message['state'] = 'unExceptPath'
-            import datetime
-            message['date'] = datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
-            message['bodyMessage'] = b''
-            message['others'] = ''
-            return message
+            return self.err404()
+
 
     def err404(self):
         message = {}
